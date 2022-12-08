584513d0060b250867301c706f8b97c569e0e0ac
lib678 <lib678@newschool.edu>
2022-12-08 15:34:53 -0500

    Add button behavior to Scene 2

--- Emo Bunny/Assets/Script/buttonBehavior.cs
+++ Emo Bunny/Assets/Script/buttonBehavior.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class buttonBehavior : MonoBehaviour
 {
+    public GameObject targetPanel;
+    public GameObject currentPanel;
+    public int targetScene;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void ChangeScene()
+    {
+
+        SceneManager.LoadScene(targetScene);
+    }
+    public void ChangePanel()
+    {
+
+        targetPanel.SetActive(true);
+        currentPanel.SetActive(false);
     }
 }
