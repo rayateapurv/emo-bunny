fe0c16600ca97f41f9d9f0341e66ccabcc8bd8f9
Emily Jin <jinc856@newschool.edu>
2023-03-17 12:25:19 -0400

    Finish enabling feeding panel when touch bunny

--- Emo Bunny/Assets/Script/TouchBunny.cs
+++ Emo Bunny/Assets/Script/TouchBunny.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class TouchBunny : MonoBehaviour
 {
     public GameObject FeedingPanel;
+    
 
-    public void whenButtonClicked()
+    void Update()
     {
-        if (FeedingPanel.activeInHierarchy == true)
-            FeedingPanel.SetActive(false);
-        else
-            FeedingPanel.SetActive(true);
-    }
+        if (Input.touchCount > 0 && Input.touches[1].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[1].position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    if (FeedingPanel != null)
+                    {
+                        FeedingPanel.SetActive(true);
+                    }
+                }
+            }
+        }
+
+    }   
    
 }
